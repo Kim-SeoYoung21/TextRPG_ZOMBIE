a3316ff343b687539e90b8c67142590b27646715
fdfdfdfdfdfdfdfdfdfdfdfd <tbe2bv@gmail.com>
2025-01-13 19:06:10 +0900

    아이템

--- TextRPG_ZOMBIE/Item.cpp
+++ TextRPG_ZOMBIE/Item.cpp
@@ -1,46 +1,76 @@
 #include "Item.h"
 
 // 생성자 구현 (초기화 값을 넣거나, 기본값 설정)
-All_Item::All_Item()
-    : Name(""), ItemType(-1), Price(0), CurrentStack(0), MaxStack(0)
+All_Item::All_Item(ItemList type)
+    : Name(""), ItemType(type), Price(0), CurrentStack(0), MaxStack(0)
 {
+    switch (ItemType)
+    {
+    case Morphine:
+        Name = "모르핀";
+        Price = 50;
+        MaxStack = 5;
+        break;
+
+    case Epinephrine:
+        Name = "에피네프린";
+        Price = 60;
+        MaxStack = 1;
+        break;
+
+    case Methylamphetamine:
+        Name = "메스암페타민";
+        Price = 70;
+        MaxStack = 1;
+        break;
+
+    case Fentanyl:
+        Name = "펜타닐";
+        Price = 80;
+        MaxStack = 2;
+        break;
+
+    case Revital:
+        Name = "레비탈";
+        Price = 100;
+        MaxStack = 1;
+        break;
+    }
 }
 
 void All_Item::use(Character* chara)
 {
-    // 아이템 종류(ItemType)에 따른 효과를 switch문으로 구현
     switch (ItemType)
     {
     case Morphine:
-        // 예: 체력 50 회복
         cout << "사용: 모르핀, 체력 50 회복.";
         chara->HP += 50;
         if (chara->HP > chara->MaxHP)
             chara->HP = chara->MaxHP;
         break;
+
     case Epinephrine:
         cout << "사용: 에피네프린, 공격력 10 증가.";
-        chara->HP += 30;
-        if (chara->HP > chara->MaxHP)
-            chara->HP = chara->MaxHP;
+        chara->AttackPower += 10;
         break;
+
     case Methylamphetamine:
         cout << "사용: 메스암페타민, 공격력 50 증가.";
-        chara->HP += 30;
-        if (chara->HP > chara->MaxHP)
-            chara->HP = chara->MaxHP;
+        chara->AttackPower += 50;
         break;
+
     case Fentanyl:
         cout << "사용: 펜타닐, 최대 체력 30 감소, 체력 5000 회복";
-        chara->HP += 30;
+        chara->MaxHP -= 30;
+        chara->HP += 5000;
         if (chara->HP > chara->MaxHP)
             chara->HP = chara->MaxHP;
         break;
+
     case Revital:
         cout << "사용: 레비탈, 최대 체력 80 증가";
-        chara->HP += 30;
-        if (chara->HP > chara->MaxHP)
-            chara->HP = chara->MaxHP;
+        chara->MaxHP += 80;
+        chara->HP += 80;
         break;
     }
 
--- TextRPG_ZOMBIE/Item.h
+++ TextRPG_ZOMBIE/Item.h
@@ -1,12 +1,8 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <cstdlib> // 난수 생성을 위한 헤더 파일
-#include <ctime> // 시간 관련 함수를 위한 헤더 파일
 #include "Actor.h"
 #include "Character.h"
-// 몬스터 파일(몬스터 아이템 드랍 관련 연동용)
-// 상점 파일?
 
 using namespace std;
 
@@ -20,8 +16,6 @@ enum ItemList // 모든 아이템 목록
 	Max
 };
 
-class Character;
-
 class All_Item // 아이템 설정 및 사용효과
 {
 protected:
@@ -31,54 +25,20 @@ protected:
 	int CurrentStack;
 	int MaxStack;
 public: 
-	virtual void use(Character* chara){}
+
+	All_Item(ItemList type);
+
+	virtual void use(Character* chara);
 };
 
-class Inventory
+class Inventory 
 {
 public:
 	All_Item item[ItemList::Max];
 };
 
-class MonsterDropItem // 몬스터 하위 클래스 *아이템드랍테이블
+class MonsterDropItem 
 {
 public:
-	void dropitem(Inventory* Inven)
-	{ 
-		// 몬스터 사망값? 작업 공유
-		//if (isdead() == true)
-		//{
-		//	int droptable = 0;
-
-		//	srand((unsigned int)time(null)); // 랜덤 시드 초기화
-
-		//	droptable = rand() % 101; // 0~100까지 랜덤
-
-		//	if (droptable < 31)
-		//	{
-		//		inven.item[morphine].currentstack += 1;
-		//	}
-		//	else if (30 < droptable && droptable < 51)
-		//	{
-		//		item[epinephrine].currentstack += 1;
-		//	}
-		//	else if (50 < droptable && droptable < 61)
-		//	{
-		//		item[methylamphetamine].currentstack += 1;
-		//	}
-		//	else if (60 < droptable && droptable < 71)
-		//	{
-		//		item[fentanyl].currentstack += 1;
-		//	}
-		//	else if (70 < droptable && droptable < 76)
-		//	{
-		//		item[revital].currentstack += 1;
-		//	}
-		//	else if (75 < droptable && droptable < 81)
-		//	{
-		//		item[morphine].currentstack += 1;
-		//		item[epinephrine].currentstack += 1;
-		//	}
-		//}
-	}
+	void dropitem(Inventory* Inven);
 };
